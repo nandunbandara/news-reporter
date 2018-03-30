e505a43f127fbdc8104f02984058a522a4a8f0bb
Nandun Bandara <ntbandara3@hotmail.com>
2018-03-30 10:31:20 +0530

    Firebase integration and login implemented.

--- app/src/main/java/me/nandunb/newsreporter/LoginActivity.java
+++ app/src/main/java/me/nandunb/newsreporter/LoginActivity.java
@@ -1,13 +1,75 @@
 package me.nandunb.newsreporter;
 
+import android.content.Intent;
+import android.os.Parcelable;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private static final String TAG = "NR_DEBUG";
+
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        mAuth = FirebaseAuth.getInstance();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        //Check if the user is logged in and go to feed
+//        FirebaseUser currentUser = mAuth.getCurrentUser();
+//        if(currentUser != null){
+//            updateUI(currentUser);
+//        }
+    }
+
+    public void signin(View view) {
+
+        EditText emailText = (EditText) findViewById(R.id.user_email);
+        EditText passwordText = (EditText) findViewById(R.id.user_password);
+
+        String email = emailText.getText().toString();
+        String password = passwordText.getText().toString();
+
+        mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful()) {
+                    Log.d(TAG, "signInWithEmailAndPassword: success");
+                    FirebaseUser user = mAuth.getCurrentUser();
+                    Toast.makeText(LoginActivity.this, "Login successful!", Toast.LENGTH_SHORT).show();
+                    updateUI(user);
+                }else{
+                    Log.w(TAG, "signInWithEmailAndPassword: failure");
+                    Toast.makeText(LoginActivity.this, "Login failed!", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+    }
+
+    public void updateUI(FirebaseUser user){
+        Intent intent = new Intent(this, NewsFeed.class);
+        intent.putExtra("user", (Parcelable) user);
+        startActivity(intent);
     }
 }
