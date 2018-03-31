497180ee422e9ee60c25bebe0704d0c058462121
Nandun Bandara <ntbandara3@hotmail.com>
2018-03-31 15:25:52 +0530

    Sign Up functionality implemented

--- app/src/main/java/me/nandunb/newsreporter/SignUpActivity.java
+++ app/src/main/java/me/nandunb/newsreporter/SignUpActivity.java
@@ -1,20 +1,94 @@
 package me.nandunb.newsreporter;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.UserProfileChangeRequest;
 
 public class SignUpActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
+
+    static final String TAG = "NR_DEBUG";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        mAuth = FirebaseAuth.getInstance();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+
+        if(currentUser != null){
+            updateUI(currentUser);
+        }
+    }
+
+    public void updateUI(FirebaseUser user){
+        Intent intent = new Intent(this, NewsFeedActivity.class);
+        intent.putExtra("displayName", user.getDisplayName());
+        intent.putExtra("email", user.getEmail());
+        startActivity(intent);
     }
 
     public void signUp(View view) {
+
+        EditText txtEmail = findViewById(R.id.user_signup_email);
+        EditText txtDisplayName = findViewById(R.id.user_signup_display_name);
+        EditText txtPassword = findViewById(R.id.user_signup_password);
+
+        String email = txtEmail.getText().toString();
+        final String displayName = txtDisplayName.getText().toString();
+        String password = txtPassword.getText().toString();
+
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()){
+                            Log.d(TAG, "createUserWithEmailAndPassword: successful");
+                            final FirebaseUser currentUser = mAuth.getCurrentUser();
+
+                            //Set display name
+                            UserProfileChangeRequest request = new UserProfileChangeRequest.Builder()
+                                    .setDisplayName(displayName)
+                                    .build();
+                            currentUser.updateProfile(request)
+                                    .addOnCompleteListener(new OnCompleteListener<Void>() {
+                                        @Override
+                                        public void onComplete(@NonNull Task<Void> task) {
+                                            Toast.makeText(SignUpActivity.this,
+                                                    "Sign Up successful!", Toast.LENGTH_SHORT);
+                                            updateUI(currentUser);
+                                        }
+                                    });
+                        }else {
+                            Log.w(TAG, "Sign Up failed");
+                            Toast.makeText(SignUpActivity.this, "Sign Up failed!", Toast.LENGTH_SHORT);
+                        }
+                    }
+                });
+
+
     }
+
 
     public void goToLogin(View view){
         Intent loginIntent = new Intent(SignUpActivity.this, SignInActivity.class);
