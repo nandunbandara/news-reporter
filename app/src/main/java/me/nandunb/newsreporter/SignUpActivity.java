a48aef5c21a496080b152fc28ac65b6ec53760de
Nandun Bandara <ntbandara3@hotmail.com>
2018-03-31 10:35:07 +0530

    Signup UI implemented

---
+++ app/src/main/java/me/nandunb/newsreporter/SignUpActivity.java
@@ -0,0 +1,17 @@
+package me.nandunb.newsreporter;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class SignUpActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+    }
+
+    public void signUp(View view) {
+    }
+}
