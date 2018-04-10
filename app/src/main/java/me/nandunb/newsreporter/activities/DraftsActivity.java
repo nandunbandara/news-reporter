5a319d9b53c7a6947094fed0ae21dd1b840a32a0
Nandun Bandara <ntbandara3@hotmail.com>
2018-04-11 01:28:10 +0530

    Drafts retrieving implemented

--- app/src/main/java/me/nandunb/newsreporter/activities/DraftsActivity.java
+++ app/src/main/java/me/nandunb/newsreporter/activities/DraftsActivity.java
@@ -4,14 +4,45 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.View;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import me.nandunb.newsreporter.R;
+import me.nandunb.newsreporter.helpers.DraftAdapter;
+import me.nandunb.newsreporter.helpers.NewsAdapter;
+import me.nandunb.newsreporter.models.Draft;
+import me.nandunb.newsreporter.models.Post;
 
 public class DraftsActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
+    private RecyclerView recyclerView;
+    private DraftAdapter adapter;
+    private List<Draft> draftList;
+
+
+    private FirebaseDatabase mDatabase;
+    private DatabaseReference ref;
+
+
+    static final String TAG = "NR_DRAFT";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +53,24 @@ public class DraftsActivity extends AppCompatActivity {
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setTitle(R.string.drafts_title);
 
+        mDatabase = FirebaseDatabase.getInstance();
+
+        ref = mDatabase.getReference("drafts");
+
+        recyclerView = (RecyclerView) findViewById(R.id.drafts_recycler_view);
+
+        draftList = new ArrayList<>();
+        adapter = new DraftAdapter(this, draftList);
+
+        RecyclerView.LayoutManager mLayoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(mLayoutManager);
+        recyclerView.setAdapter(adapter);
+
+
+        mAuth = FirebaseAuth.getInstance();
+
+        retrieve();
+
     }
 
     @Override
@@ -29,6 +78,58 @@ public class DraftsActivity extends AppCompatActivity {
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.options_menu_drafts, menu);
         return true;
+    }
+
+
+    private void retrieve(){
+
+        Log.d(TAG+" retrieve()", "Retrieving data.");
+
+        FirebaseUser user = mAuth.getCurrentUser();
+
+        Log.d(TAG, "UserID "+ user.getUid());
+
+        Query query = ref.child(user.getUid()).orderByChild("createdOn");
+
+        query.addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+                fetchData(dataSnapshot);
+            }
+
+            @Override
+            public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onChildRemoved(DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+
+
+    }
+
+    private void fetchData(DataSnapshot snapshot){
+
+        Log.d(TAG+" fetchData()", snapshot.getKey());
+
+        Draft draft = snapshot.getValue(Draft.class);
+        draftList.add(draft);
+
+        adapter.notifyDataSetChanged();
+
     }
 
 }
---
+++ app/src/main/java/me/nandunb/newsreporter/helpers/DraftAdapter.java
@@ -0,0 +1,69 @@
+package me.nandunb.newsreporter.helpers;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+
+import java.util.List;
+
+import me.nandunb.newsreporter.R;
+import me.nandunb.newsreporter.models.Draft;
+import me.nandunb.newsreporter.models.Post;
+
+/**
+ * Created by nandunb on 4/11/18.
+ */
+
+public class DraftAdapter extends RecyclerView.Adapter<DraftAdapter.DraftViewHolder> {
+
+    private Context context;
+    private List<Draft> draftList;
+
+    public DraftAdapter(Context context, List<Draft> draftList){
+        this.context = context;
+        this.draftList = draftList;
+    }
+
+    @NonNull
+    @Override
+    public DraftViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View itemView = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.news_card_layout, parent, false);
+
+        return new DraftViewHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull DraftAdapter.DraftViewHolder holder, int position) {
+        Draft draft = draftList.get(position);
+        holder.datetime.setText(draft.getDateTime());
+        holder.caption.setText(draft.getCaption());
+
+        Glide.with(context).load(draft.getPhotoUrl()).into(holder.image);
+    }
+
+    @Override
+    public int getItemCount() {
+        return draftList.size();
+    }
+
+    public class DraftViewHolder extends RecyclerView.ViewHolder {
+        public TextView caption, datetime;
+        public ImageView image;
+
+        public DraftViewHolder(View view) {
+            super(view);
+            caption = (TextView) view.findViewById(R.id.txtCardCaption);
+            image = (ImageView) view.findViewById(R.id.card_image);
+            datetime = (TextView) view.findViewById(R.id.card_date);
+        }
+    }
+
+}
