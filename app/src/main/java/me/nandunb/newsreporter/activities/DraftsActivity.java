cbb0de6dabcf10fce297143a2ac0c95211795350
Nandun Bandara <ntbandara3@hotmail.com>
2018-04-10 23:54:52 +0530

    Drafts UI navigation and enhancements

--- app/src/main/java/me/nandunb/newsreporter/activities/DraftsActivity.java
+++ app/src/main/java/me/nandunb/newsreporter/activities/DraftsActivity.java
@@ -5,6 +5,8 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 
 import me.nandunb.newsreporter.R;
@@ -17,15 +19,16 @@ public class DraftsActivity extends AppCompatActivity {
         setContentView(R.layout.activity_drafts);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setTitle(R.string.drafts_title);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.options_menu_drafts, menu);
+        return true;
     }
 
 }
